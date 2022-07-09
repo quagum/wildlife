b38faf084e7f4dca3b8960cdb476c7c822222838
Charles Wang <quagum125@gmail.com>
2022-07-08 20:57:11 -0400

    formatted return type for mySQL query

--- functions.js
+++ functions.js
@@ -1,25 +1,26 @@
 const mysql = require('mysql')
 
-const con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "Turtle415_",
-    database:"wildlife"
-    });
-
 function getInformation(){
-    var str = document.getElementById("searchText").value;
-    alert("value inside the text box is: " + str);
+    const con = mysql.createConnection({
+        host: "localhost",
+        user: "root",
+        password: "Turtle415_",
+        database:"wildlife"
+        });
+    con.connect(function(err){
+        if(err) throw err;
+        console.log('CONNECTED TO MYSQL DB')
+        con.query("SELECT * FROM animals;", function(err, result, fields){
+            if(err) throw err;
+            console.log(result[0].ID); //result is an array of object named RowDataPacket 
+            return result[0].ID;
+        });
+    });
 }
 
-con.connect(function(err){
-    if(err) throw err;
-    console.log('CONNECTED')
-    con.query("SELECT * FROM animals;", function(err, result, fields){
-        if(err) throw err;
-        console.log(result);
-    });
-});
+
+module.exports = {getInformation};
+
 
 
 
