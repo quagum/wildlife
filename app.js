e48e897e8ec0066cf9a6386a37768eaa35f3ee2e
Charles Wang <quagum125@gmail.com>
2022-07-12 12:54:04 -0400

    visibility

--- app.js
+++ app.js
@@ -3,20 +3,28 @@ const func = require("./functions.js");
 
 const app = express();
 
-app.listen(3000, function () {
-    console.log("Server is running on localhost3000");
-});
+app.listen(3000, 
+    function(){
+        console.log("Server is running on localhost3000");
+    }
+);
 
-app.get("/", function(req, res) {
-    res.sendFile(__dirname + "/index.html");
-});
+app.get("/", 
+    function(req, res){
+        res.sendFile(__dirname + "/index.html");
+    }
+);
 
-app.get("/view", function(req, res){
-    res.sendFile(__dirname + "/view.html");
-    func.getInformation();
-})
+app.get("/view", 
+    function(req, res){
+        res.sendFile(__dirname + "/view.html");
+        func.getInformation();
+    }
+);
 
-app.get("/submission", function(req, res){
-    res.sendFile(__dirname + "/submission.html");
-    func.addInformation("BBBBB", "Female"); 
-})
+app.get("/submission", 
+    function(req, res){
+        res.sendFile(__dirname + "/submission.html");
+        func.addInformation("BBBBB", "Female"); 
+    }  
+);
