eb64ff9acd2d9c910511c38550cb2cde96145c83
Charles Wang <quagum125@gmail.com>
2022-07-13 22:16:09 -0400

    testing with ejs

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const func = require("./functions.js");
 
 const app = express();
 
+var bodyParser = require('body-parser')
+
+app.use(express.static(__dirname + '/'));
+app.use(bodyParser.urlencoded({extend:true}));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine','ejs');
+app.set('views', __dirname)
+
 app.listen(3000, 
     function(){
         console.log("Server is running on localhost3000");
@@ -17,8 +25,10 @@ app.get("/",
 
 app.get("/view", 
     function(req, res){
-        res.sendFile(__dirname + "/view.html");
-        func.getInformation();
+        //res.sendFile(__dirname + "/view.html");
+        data = func.getInformation();
+        console.log(data)
+        res.render('view.ejs', {data: data});
     }
 );
 
