714df0db3fcc05e8b66d9329ec2a071e528f815b
Charles Wang <quagum125@gmail.com>
2022-07-22 18:38:41 -0400

    creation of animals routes

---
+++ routes/animals.js
@@ -0,0 +1,19 @@
+const { json } = require("body-parser");
+const express = require("express");
+const router = express.Router();
+const func = require("../functions.js");
+
+router.get('/', async function(req, res){
+    var data = await func.getIDs();
+    var jsonData = JSON.parse(JSON.stringify(data));
+    res.json(jsonData);
+})
+
+router.get('/:animalID', async function(req, res){
+    var animalID = req.params.animalID;
+    var data = await func.getGPS(animalID);
+    var jsonData = JSON.parse(JSON.stringify(data));
+    res.json(jsonData);
+})
+
+module.exports = router 
