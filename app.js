88d7feec8e5d479a20c6634e300978eb7b06a4e4
Charles Wang <quagum125@gmail.com>
2022-07-29 14:42:21 -0400

    accounted for column changes

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
-//import of express webapp
 const express = require("express");
-//creation of express object called app which is able to handle HTTP requests
 const app = express();
 
-//app object acknowledging server hosted on http://localhost:3000
 app.listen(3000, function(){
     console.log("Server is running on http://localhost:3000");
 });
